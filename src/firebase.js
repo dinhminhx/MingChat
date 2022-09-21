9b635a9740d2dea5b799f833547fe4e9338c409b
minhnd-0256 <nghiem.dinh.minh@sun-asterisk.com>
2022-09-21 22:57:14 +0700

    Update feature Login Register Logout

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ3A_8X1OWQarhUDA-iLMjeABkmbky0s4",
+  authDomain: "mingchat-f42e8.firebaseapp.com",
+  projectId: "mingchat-f42e8",
+  storageBucket: "mingchat-f42e8.appspot.com",
+  messagingSenderId: "802830141402",
+  appId: "1:802830141402:web:25728b720bd088424ca2e3",
+  measurementId: "G-LR2P8LJE8R"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
+const analytics = getAnalytics(app);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
 const Login = () => {
+
+  const navigate = useNavigate();
+  const [error,setError] = useState(false)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+      navigate("/")
+    } catch (error) {
+      setError(true)
+    }
+  }
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Ming Chat</span>
         <span className="title">Login</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <input type="file" id="file" style={{display: 'none'}} />
           <button>Login</button>
+          {error && <span>Something Wrong Here ?</span>}
         </form>
-        <p>You don't have account ? Sign up here</p>
+        <p>You don't have account ? <Link to="/register">Sign up here</Link></p>
       </div>
     </div>
   )
